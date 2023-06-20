70ad6fdfbe9a22d1e7dd76804120af0a8316ec7b
Prakerja <prakerja@Prakerjas-MacBook-Pro-5.local>
2023-06-20 15:13:23 +0700

    add header & footer menu for language

--- js/publikasi.js
+++ js/publikasi.js
@@ -1,27 +1,46 @@
 (function ($) {window.fnames = new Array(); window.ftypes = new Array(); fnames[0] = 'EMAIL'; ftypes[0] = 'email'; fnames[1] = 'FNAME'; ftypes[1] = 'text'; fnames[2] = 'LNAME'; ftypes[2] = 'text'; fnames[6] = 'FUNCTION'; ftypes[6] = 'dropdown'; fnames[3] = 'COMPANY'; ftypes[3] = 'text'; fnames[4] = 'TITLE'; ftypes[4] = 'text'; }(jQuery)); 
 var $mcj = jQuery.noConflict(true);
-/* on scroll functon */
-$(window).scroll(function() {
-    var scroll = $(window).scrollTop();
-    if (scroll >= 60) {
-        $('header').addClass("header-fixed");
-    } else {
-        $('header').removeClass("header-fixed");
-    }
 
-    // for scroll-top trigger
-    if (scroll >= 400) {
-        $('.scroll-top').addClass("is-show");
-    } else {
-        $('.scroll-top').removeClass("is-show");
-    }
+/** init function */
+(function($){
+    // scroll function
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        if (scroll >= 60) {
+            $('header').addClass("header-fixed");
+        } else {
+            $('header').removeClass("header-fixed");
+        }
+
+        // for scroll-top trigger
+        if (scroll >= 400) {
+            $('.scroll-top').addClass("is-show");
+        } else {
+            $('.scroll-top').removeClass("is-show");
+        }
+
+    });
 
     // Scroll to top 
     $(".scroll-top").on("click", function() {
         $(window).scrollTop(0);
     });
 
-});
+    // Menu toggle
+    $('.menu').click (function(){
+        $(this).toggleClass('open');
+        $('.navbar-custom').toggleClass('m-menu');
+        $('body').toggleClass('freeze');
+      });
+
+      // close modal when a menu clicked
+      $(".navbar-custom").on("click", ".nav-link", function(event){
+        $('.menu').removeClass('open');
+        $('.navbar-custom').removeClass('m-menu');
+        $('body').removeClass('freeze');
+    });
+    
+ })(jQuery);
 
 /* form on submit */
 $('.button.btn-primary').on("click", function(e,f) {
